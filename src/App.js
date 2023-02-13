1bb848fb1f50ea486f902a7e6bbd8f1c13ca7a96
Spoorthi_Shet <79088643+spoorthi69@users.noreply.github.com>
2023-02-14 01:20:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+const App= () => {
+    return (
+        <ChatEngine
+          height="100vh"
+          projectID= "ace13fa7-1f10-4c25-986d-3c75a9e761a1"
+          userName="Spoorthi"
+          userSecret="123123"
+        />
+    );
+}
+export default App;
+
